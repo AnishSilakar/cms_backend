f25a6f0845031d90f40714989c20afa3bf855b4c
Anish Silakar <anishsilakar5@gmail.com>
2025-01-12 15:26:39 +0545

    Menu Group Completed

--- routes/menugroup.route.js
+++ routes/menugroup.route.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const MenuGroupController = require("../controller/menugroup.controller");
+const authMiddleware = require("../middleware/auth.middleware");
 
-router.post("/", MenuGroupController.insert);
+router.post("/", authMiddleware, MenuGroupController.insert);
 router.get("/", MenuGroupController.getAll);
-router.put("/:id", MenuGroupController.update);
-router.delete("/:id", MenuGroupController.delete);
+router.put("/:id", authMiddleware, MenuGroupController.update);
+router.delete("/:id", authMiddleware, MenuGroupController.delete);
 
 module.exports = router;
