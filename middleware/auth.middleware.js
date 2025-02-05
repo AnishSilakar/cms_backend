4fc5bb18cebe1ac632a9d9968dfd3967e65fa2b7
Anish Silakar <anishsilakar5@gmail.com>
2025-02-05 11:09:04 +0545

    simple

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -4,19 +4,19 @@ const models = require("../models");
 const authMiddleware = async (req, res, next) => {
     const accessToken = req.headers['authorization']?.split(' ')[1];
     if (!accessToken) {
-        return res.status(403).json({"message": "No token provided"});
+        return res.status(401).json({"message": "No token provided"});
     }
     const tokens = await models.TokenManager.findOne({where: {accessToken}});
     if (!tokens) {
-        return res.status(403).json({"message": "Invalid Access Token"});
+        return res.status(401).json({"message": "Invalid Access Token"});
     }
     const now = new Date(Date.now());
     if (now > new Date(tokens.accessExpiresIn)) {
-        return res.status(403).json({"message": "Access Token is Expired"});
+        return res.status(401).json({"message": "Access Token is Expired"});
     }
     jwt.verify(accessToken, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(403).json({"message": "Invalid token provided"});
+            return res.status(401).json({"message": "Invalid token provided"});
         }
         req.userId = decoded.id; // Store user ID for later use
         next();
--- routes/pageSection.route.js
+++ routes/pageSection.route.js
@@ -2,13 +2,11 @@ const router = require("express").Router();
 const pageSectionController = require("../controller/pageSection.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
-router.use(authMiddleware);
-
-router.get("/", pageSectionController.getAll);
-router.get("/getPages", pageSectionController.getPages);
-router.post("/", pageSectionController.insert);
-router.post("/update", pageSectionController.update);
-router.delete("/:id", pageSectionController.remove);
+router.get("/", authMiddleware, pageSectionController.getAll);
+router.get("/getPages", authMiddleware, pageSectionController.getPages);
+router.post("/", authMiddleware, pageSectionController.insert);
+router.post("/update", authMiddleware, pageSectionController.update);
+router.delete("/:id", authMiddleware, pageSectionController.remove);
 router.get("/selectByPageId/:pageId", pageSectionController.selectByPageId);
 
 module.exports = router;
