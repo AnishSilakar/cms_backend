516dfba1335e69060b7336dffa6a2f2e2ab1a867
Anish Silakar <aomineshooter@gmail.com>
2025-04-24 16:01:31 +0545

    simple

--- middleware/requestInterceptor.middleware.js
+++ middleware/requestInterceptor.middleware.js
@@ -19,6 +19,18 @@ const httpMethodInterceptor = async (req, res, next) => {
             }
         }
     }
+    else if (req.method === 'POST') {
+        const keys = await CacheService.getAllKeys();
+        if (keys.length > 0) {
+            for (const key of keys) {
+                if (key.includes('central:moduleCacheKey-')) {
+                    const getColonIndex = key.indexOf(':');
+                    const newKey = key.substring(getColonIndex + 1);
+                    await CacheService.del(newKey);               
+                }
+            }
+        }
+    }
     next();
 }
 
