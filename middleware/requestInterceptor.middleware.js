f828412202cfba7da0753cc79905d3e00be90b02
Anish Silakar <aomineshooter@gmail.com>
2025-04-24 16:13:17 +0545

    simple

--- middleware/requestInterceptor.middleware.js
+++ middleware/requestInterceptor.middleware.js
@@ -1,32 +1,29 @@
 const redis = require('redis');
 const CacheService = require('../service/cache.service');
 
+const deleteModuleCache = async (key) => {
+    const getColonIndex = key.indexOf(':');
+    const newKey = key.substring(getColonIndex + 1);
+    await CacheService.del(newKey);
+}
+
 const httpMethodInterceptor = async (req, res, next) => {
-    if (req.method === 'PUT' || req.method === 'DELETE') {
-        const keys = await CacheService.getAllKeys();
-        if (keys.length > 0) {
-            for (const key of keys) {
+    const keys = await CacheService.getAllKeys();
+    if (keys.length > 0) {
+        for (const key of keys) {
+            if (req.method === 'PUT' || req.method === 'DELETE') {
                 if (key.includes('central:pageSectionCacheKey-')) {
                     const getColonIndex = key.indexOf(':');
                     const newKey = key.substring(getColonIndex + 1);
-                    await CacheService.del(newKey);               
+                    await CacheService.del(newKey);
                 }
                 if (key.includes('central:moduleCacheKey-')) {
-                    const getColonIndex = key.indexOf(':');
-                    const newKey = key.substring(getColonIndex + 1);
-                    await CacheService.del(newKey);               
+                    await deleteModuleCache(key);
                 }
             }
-        }
-    }
-    else if (req.method === 'POST') {
-        const keys = await CacheService.getAllKeys();
-        if (keys.length > 0) {
-            for (const key of keys) {
+            else if (req.method === 'POST') {
                 if (key.includes('central:moduleCacheKey-')) {
-                    const getColonIndex = key.indexOf(':');
-                    const newKey = key.substring(getColonIndex + 1);
-                    await CacheService.del(newKey);               
+                    await deleteModuleCache(key);
                 }
             }
         }
